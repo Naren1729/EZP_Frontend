7aa7b1771329d995b386c10671afa3d3fe600b2a
Bhavansh-01 <galibhavansh2003@gmail.com>
2024-09-01 23:01:39 +0530

    Added FetchingData page

--- ezp-frontend/src/App.js
+++ ezp-frontend/src/App.js
@@ -7,11 +7,13 @@ import UserForm from './Pages/UserForm';
 import AdminAccess from './Pages/AdminAccess';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import FetchingData from './Pages/FetchingData';
 
 //Creating an array of object
 const routerConfig = createBrowserRouter([
     {path:"/", element:<Home/> },
     {path: "/main/authenticate", element:<UserLogin/> },
+    {path: "/admin/data", element:<FetchingData/>},
     {path: "/admin/authenticate", element:<AdminLogin/> },
     {path: "/main/userForm", element:<UserForm/> },
     {path: "/admin/adminAccess", element:<AdminAccess/> },
