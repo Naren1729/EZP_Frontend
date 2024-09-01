f2fc37aa5164451fc985a4c7aebf5530447476c3
Mayuri13 <mayuri.goswami2017@gmail.com>
2024-09-01 22:31:59 +0530

    User_Form_Added

---
+++ ezp-frontend/src/Pages/AdminAccess.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function AdminAccess() {
+  return (
+    <div className="admin-container">
+      <div className="subContainer">
+          <p >Get users data</p>
+      </div>
+      <div className="subContainer">
+          <p>Get riskScores</p>
+      </div>
+      
+    </div>
+  )
+}
