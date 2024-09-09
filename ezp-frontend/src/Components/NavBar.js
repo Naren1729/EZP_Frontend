80f905adee0ee8268e1a8c412e4277bdf15fb78a
Mayuri13 <mayuri.goswami2017@gmail.com>
2024-09-09 15:39:43 +0530

    React scroll duration updated

--- ezp-frontend/src/Components/NavBar.js
+++ ezp-frontend/src/Components/NavBar.js
@@ -6,18 +6,18 @@ export default function NavBar() {
     <div className="nav-bar">
       <ul className="nav-items">
         <li>
-          <Link to="/" spy={true} smooth={true} offset={-100} duration={500}>
+          <Link to="/" spy={true} smooth={true} offset={-100} duration={1000}>
             Home
           </Link>
         </li>
         <li>
-          <Link to="about" spy={true} smooth={true} offset={-150} duration={500}>About</Link>
+          <Link to="about" spy={true} smooth={true} offset={-150} duration={1000}>About</Link>
         </li>
         <li>
-          <Link to="features" spy={true} smooth={true} offset={-150} duration={500}>Features</Link>
+          <Link to="features" spy={true} smooth={true} offset={-150} duration={1000}>Features</Link>
         </li>
         <li>
-          <Link to="team" spy={true} smooth={true} offset={-20} duration={500}>Our Team</Link>
+          <Link to="team" spy={true} smooth={true} offset={-20} duration={1000}>Our Team</Link>
         </li>
       </ul>
     </div>
